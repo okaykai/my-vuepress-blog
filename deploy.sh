9f6ae24e20385e1c374c9cd3ce5e1e9b4b89ac77
okaykai <wangwk1998@qq.com>
2022-11-18 16:25:44 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:okaykai/my-vuepress-blog.git master:gh-pages
+git push -f git@github.com:okaykai/my-vuepress-blog.git main:gh-pages
 
 cd -
